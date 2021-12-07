022613365d5865c63bbd7a0e747554a1969917e7
charj99 <charj99@163.com>
2021-12-07 21:54:51 +0800

    final commit, if touch it again, im a dog

--- assignment3/src/utils.h
+++ assignment3/src/utils.h
@@ -20,7 +20,7 @@ typedef std::map<int, FuncSet, std::less<int>> Int2FuncSetMap;
 typedef std::map<llvm::Value*, llvm::Value*> V2VMap;
 typedef std::map<llvm::Function*, llvm::BasicBlock*> Func2BBMap;
 typedef std::map<ValueSet, ValueSet> VSet2VSetMap;
-#define DEBUG 1
+#define DEBUG 0
 #define Diag if (DEBUG) llvm::errs()
 #define CONTAINS(container, key) \
     (container.find(key) != container.end())
